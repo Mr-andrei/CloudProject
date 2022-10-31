6aef1c4d42b34cd0ff33db57675323425f948e33
Andrei <91520213+Mr-andrei@users.noreply.github.com>
2022-10-31 20:07:13 +0300

    Add Prettier and  Eslint

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,22 +1,22 @@
-import React, {useEffect} from 'react';
-import './app.css'
-import RouteFile from "./routes/RouteFile";
-import {BrowserRouter} from "react-router-dom";
-import {useAppDispatch} from "./redux/store/store";
-import {authTC} from "./redux/thunk/logInThunks";
+import React, { useEffect } from 'react';
+import './app.css';
+import RouteFile from './routes/RouteFile';
+import { BrowserRouter } from 'react-router-dom';
+import { useAppDispatch } from './redux/store/store';
+import { authTC } from './redux/thunk/logInThunks';
 
 // #004D64
 function App() {
-    const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch();
 
-    useEffect(()=>{
-        dispatch(authTC())
-    },[])
+    useEffect(() => {
+        dispatch(authTC());
+    }, []);
 
     return (
         <div className="App">
             <BrowserRouter>
-                <RouteFile/>
+                <RouteFile />
             </BrowserRouter>
         </div>
     );
