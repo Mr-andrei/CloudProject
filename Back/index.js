7469fedfa56ffadaa4ff939e2a97f6a6ef7db9c1
Andrei <91520213+Mr-andrei@users.noreply.github.com>
2022-10-31 20:10:54 +0300

    Add Prettier and  Eslint

--- Back/index.js
+++ Back/index.js
@@ -5,18 +5,19 @@ import authRoutes from "./routes/auth.routes.js";
 import cors from "cors";
 
 const app = express();
-const PORT = config.get("serverPort");
+const PORT = config.get("serverPort")
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 const start = async () => {
-  try {
-    await mongoose.connect(config.get("DB_URL"));
-    app.listen(PORT, () => {
-      console.log("Server work on port", PORT);
-    });
-  } catch (e) {}
+    try {
+        await mongoose.connect(config.get("DB_URL"));
+        app.listen(PORT, () => {
+            console.log("Server work on port", PORT);
+        });
+    } catch (e) {
+    }
 };
 start();
